07f83152a792a96f2c085095eef6f31df29f4806
Dima12411 <fixer40delta@gmail.com>
2021-10-22 18:38:26 +0300

    solution of the task2

---
+++ task2.js
@@ -0,0 +1,12 @@
+/* task:
+Write a function to split a string and convert it into an array of words. */
+
+//code:
+
+function stringToArray(string){
+
+	let arr = string.split(' ');
+return arr;
+}
+stringToArray('Robin Singh');
+stringToArray('I love arrays they are my favorite');
