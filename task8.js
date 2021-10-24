0c6163b809ca61497d3c4ec22187a26794751840
Dima12411 <fixer40delta@gmail.com>
2021-10-24 12:57:00 +0300

    solution of the task8

---
+++ task8.js
@@ -0,0 +1,12 @@
+/* Task:
+Build a function that returns an array of integers from n to 1 where n>0. */
+
+//code:
+
+const reverseSeq = n => {
+    let arr = [];
+    for (let i = n; i > 0; i--) {
+      arr.push(i);
+    }
+    return arr;
+  };
