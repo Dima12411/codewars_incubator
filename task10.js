d5e93c85fcde014c55972c6fdfd0665a392627f4
Dima12411 <fixer40delta@gmail.com>
2021-10-24 13:06:02 +0300

    solution of the task10

---
+++ task10.js
@@ -0,0 +1,11 @@
+/* Task:
+This kata is inspired by Convert a Number to a String!. Try that one too. */
+
+
+
+//code:
+
+var stringToNumber = function(str){
+    let num = Number(str);
+    return num;
+  }
