d4e33b957b1ea1c5b2cce8776a02a9af89032872
Dima12411 <fixer40delta@gmail.com>
2021-10-25 17:32:34 +0300

    solution of task20

---
+++ task20.js
@@ -0,0 +1,20 @@
+/* Task:
+In this Kata we are passing a number (n) into a function.
+
+Your code will determine if the number passed is even (or not).
+
+The function needs to return either a true or false.
+
+Numbers may be positive or negative, integers or floats.
+
+Floats are considered UNeven for this kata. */
+
+//code:
+
+function testEven(n) {
+    if (n % 2 == 0) {
+      return true;
+    } else {
+      return false;
+    }
+}
