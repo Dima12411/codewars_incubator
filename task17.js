198930ecaafd115435347f0fd639000190037ec7
Dima12411 <fixer40delta@gmail.com>
2021-10-25 17:14:08 +0300

    solution of the task17

---
+++ task17.js
@@ -0,0 +1,12 @@
+/* Task:
+You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false */
+
+//code:
+
+function smallEnough(a, limit){
+    if (limit >= Math.max(...a)) {
+        return true;
+    } else {
+        return false;
+    }
+}
