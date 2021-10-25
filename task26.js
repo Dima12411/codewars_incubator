22a9e76b48bb57ace3f99dbaba3dfd4f9bc2f354
Dima12411 <fixer40delta@gmail.com>
2021-10-25 19:29:56 +0300

    solution of the task26

---
+++ task26.js
@@ -0,0 +1,18 @@
+/* Task:
+Your task is to make two functions, max and min
+(maximum and minimum in PHP and Python, maxi and mini in Julia) 
+that take a(n) array/vector of integers list as input and outputs, respectively, 
+the largest and lowest number in that array/vector */
+
+//code:
+
+var min = function(list){
+
+	return Math.min(...list);
+	}
+	
+	var max = function(list){
+	
+	return Math.max(...list);
+	
+	}
