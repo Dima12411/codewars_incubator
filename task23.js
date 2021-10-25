7c0dec5317f13eb09689a2660f30d249693b37ff
Dima12411 <fixer40delta@gmail.com>
2021-10-25 18:01:50 +0300

    solution of the task23

---
+++ task23.js
@@ -0,0 +1,15 @@
+/* Task:
+Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0 */
+
+//code:
+
+function paperwork(n, m) {
+    let page = 0;
+    if (n < 0 || m < 0) {
+      return 0;
+    } else {
+      return page = m * n;
+    }
+  }
