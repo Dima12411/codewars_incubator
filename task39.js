4783b1300c5ec311fb22e9e90e4b084ce8caf9f0
Dima12411 <fixer40delta@gmail.com>
2021-10-26 20:06:24 +0300

    solution of the task39.js

---
+++ task39.js
@@ -0,0 +1,12 @@
+/* Task:
+Implement a function which convert the given boolean value into its string representation. */
+
+//code:
+
+function booleanToString(b){
+    if (b == true) {
+      return "true";
+    } else {
+      return "false"
+    }
+    }
