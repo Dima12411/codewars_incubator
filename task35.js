405f16522254514443515e72effd7a227638b818
Dima12411 <fixer40delta@gmail.com>
2021-10-26 19:35:18 +0300

    solution of the task35.js

---
+++ task35.js
@@ -0,0 +1,9 @@
+/* Task:
+I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number. */
+
+//code:
+
+function addFive(num) {
+    var total = num + 5
+    return total
+  }
