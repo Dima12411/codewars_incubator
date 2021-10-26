3b13c5219e7875de6e2f35dd97fdd62ebf167e50
Dima12411 <fixer40delta@gmail.com>
2021-10-26 20:41:44 +0300

    solution of the task45.js

---
+++ task45.js
@@ -0,0 +1,11 @@
+/* Task:
+Bob needs a fast way to calculate the volume of a cuboid with three values: length, width and the height of the cuboid. Write a function to help Bob with this calculation. */
+
+//code:
+
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+     let V = length * width * height;
+      return V;
+    }
+  }
