d348afe3ed25dd5876aaade6f01fb448afe2efd2
Dima12411 <fixer40delta@gmail.com>
2021-10-26 18:45:11 +0300

    solution of the task30.js

---
+++ task30.js
@@ -0,0 +1,12 @@
+/* Task:
+Create a function that gives a personalized greeting. This function takes two parameters: name and owner */
+
+//code:
+
+function greet (name, owner) {
+    if (name == owner) {
+      return "Hello boss"
+    } else {
+      return "Hello guest"
+    }
+  }
