10cf4f9af2323ed8c841f22b7678081103f58a5a
Dima12411 <fixer40delta@gmail.com>
2021-10-27 17:40:24 +0300

    solution of the task51.js

---
+++ task51.js
@@ -0,0 +1,9 @@
+/* Task:
+Complete the solution so that it reverses the string passed into it. */
+
+//code:
+
+function solution(str){
+    let result = str.split('').reverse().join('');
+      return result;
+    }
