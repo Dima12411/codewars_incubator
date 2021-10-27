4e029bbe005d79520e9db08f979e47a9c0d84676
Dima12411 <fixer40delta@gmail.com>
2021-10-27 18:10:54 +0300

    solution of the task55.js

---
+++ task55.js
@@ -0,0 +1,16 @@
+/* Task:
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types. */
+
+//code:
+
+function findShort(s) {
+    let res = s.split(' ');
+    let a = Infinity;
+    
+    for (let i = 0; i < res.length; i++) {
+      a = Math.min(res[i].length, a);
+    }
+    return a;
+  }
