5f3b2b90f653c27ad4f091c35a12dfb960424d82
Dima12411 <fixer40delta@gmail.com>
2021-10-28 19:23:03 +0300

    solution of the task66

---
+++ task66.js
@@ -0,0 +1,10 @@
+/* Task:
+Create a function that accepts 2 string arguments and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+
+If no occurrences can be found, a count of 0 should be returned. */
+
+//code:
+
+function strCount(str, letter){  
+    return str.split(letter).length - 1;
+  }
