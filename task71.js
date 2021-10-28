c6807670445f86e6be4a57946cefb1bda09e05b3
Dima12411 <fixer40delta@gmail.com>
2021-10-28 21:48:24 +0300

    solution of the task71

---
+++ task71.js
@@ -0,0 +1,12 @@
+/* Task:
+Your job is simple, if (x) squared is more than 1000, return 'It's hotter than the sun!!', else, return 'Help yourself to a honeycomb Yorkie for the glovebox.'. */
+
+//code:
+
+function apple(x){
+    if (Number(x ** 2) > 1000) {
+      return 'It\'s hotter than the sun!!';
+    } else {
+      return 'Help yourself to a honeycomb Yorkie for the glovebox.'
+    }
+    }
