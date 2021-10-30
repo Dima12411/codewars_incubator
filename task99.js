e212ae9f2f80f8ed911b6e5b0651cc3d2a2b5f5a
Dima12411 <fixer40delta@gmail.com>
2021-10-30 21:12:08 +0300

    solution of the task99

---
+++ task99.js
@@ -0,0 +1,14 @@
+/* Task:
+Take an array and remove every second element from the array. Always keep the first element and start removing with the next element. */
+
+//code:
+
+function removeEveryOther(arr){
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (i % 2 == 0) {
+        newArr.push(arr[i]);
+      }
+    }
+    return newArr;
+  }
