d7959e82f47dec979c3136b4820678eafe76844e
Dima12411 <fixer40delta@gmail.com>
2021-10-30 18:00:22 +0300

    solution of the task93

---
+++ task93.js
@@ -0,0 +1,10 @@
+/* Task:
+We need a function that can transform a number into a string.
+
+What ways of achieving this do you know? */
+
+//code:
+
+function numberToString(num) {
+    return String(num);
+  }
